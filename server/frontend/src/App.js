75947e373873f82adc768133cec4ba6133c23198
npbaotram <np.baotram@gmail.com>
2025-04-03 13:23:38 -0500

    Mod 4: Post review

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,16 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import RegisterPanel from "./components/Register/Register"
+import Dealers from './components/Dealers/Dealers';
+import PostReview from "./components/Dealers/PostReview"
+
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
